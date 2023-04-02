cd78c2022e83fb093f7555ae31730896bf7dbd17
SamarthDambalkar7 <samarthdambalkar7@gmail.com>
2023-04-02 18:25:44 +0530

    "added default note for new student created"

--- src/main/java/com/learning/mongodbatlas/mongodbatlas/controller/CurriculumController.java
+++ src/main/java/com/learning/mongodbatlas/mongodbatlas/controller/CurriculumController.java
@@ -1,15 +1,19 @@
 package com.learning.mongodbatlas.mongodbatlas.controller;
 
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.learning.mongodbatlas.mongodbatlas.enums.Days;
 import com.learning.mongodbatlas.mongodbatlas.enums.Subjects;
+import com.learning.mongodbatlas.mongodbatlas.model.lectureTime;
 import com.learning.mongodbatlas.mongodbatlas.service.CurriculumServiceImpl;
 
 @RestController
@@ -23,4 +27,11 @@ public class CurriculumController {
     public HttpStatus addSubjectsByStd(@RequestParam int Id, @RequestParam LinkedList<Subjects> subjects) {
         return curriculumServiceImpl.addSubjectsByStd(Id, subjects);
     }
+
+    @PostMapping("/addtimetablebystd")
+    public HttpStatus addTimeTable(@RequestParam int std,
+            @RequestBody LinkedHashMap<Days, LinkedHashMap<lectureTime, Subjects>> info) {
+        return curriculumServiceImpl.setTimeTable(std, info);
+    }
+
 }
--- src/main/java/com/learning/mongodbatlas/mongodbatlas/service/CurriculumServiceImpl.java
+++ src/main/java/com/learning/mongodbatlas/mongodbatlas/service/CurriculumServiceImpl.java
@@ -1,14 +1,18 @@
 package com.learning.mongodbatlas.mongodbatlas.service;
 
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
+import java.util.Map.Entry;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.learning.mongodbatlas.mongodbatlas.enums.Days;
 import com.learning.mongodbatlas.mongodbatlas.enums.Subjects;
 import com.learning.mongodbatlas.mongodbatlas.model.Curriculum;
+import com.learning.mongodbatlas.mongodbatlas.model.lectureTime;
 import com.learning.mongodbatlas.mongodbatlas.repository.CurriculumRepository;
 
 @Service
@@ -33,6 +37,26 @@ public class CurriculumServiceImpl implements CurriculumService {
             curriculum.setStd(std);
             curriculum.setSubjects(subjects);
             curriculumRepository.save(curriculum);
+        }
+
+        return HttpStatus.OK;
+    }
+
+    @Override
+    public HttpStatus setTimeTable(int std, LinkedHashMap<Days, LinkedHashMap<lectureTime, Subjects>> info)
+            throws NoSuchElementException {
+        try {
+            Curriculum curriculum = curriculumRepository.findById(std).get();
+            LinkedHashMap<Days, LinkedHashMap<lectureTime, Subjects>> timeTable = curriculum.getTimeTable();
+            for (Entry<Days, LinkedHashMap<lectureTime, Subjects>> entry : info.entrySet()) {
+
+                timeTable.put(entry.getKey(), entry.getValue());
+            }
+            curriculum.setTimeTable(timeTable);
+            curriculumRepository.save(curriculum);
+
+        } catch (NoSuchElementException e) {
+
         }
 
         return HttpStatus.OK;
